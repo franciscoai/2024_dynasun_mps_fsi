8b747bde38350e08633b81536327da71c856f8d7
Francisco A. Iglesias <franciscoaiglesias@gmail.com>
2024-08-27 08:07:28 -0300

    first ver of func to select points, taken from  exp_sources_proyect

---
+++ select_img_points.py
@@ -0,0 +1,110 @@
+# class that takes an input list of .fits files paths  and for each file, one at a time:
+# 1- Read file as Sunpy MAPS object and plots it on screen using Sunpy built in functions (depending on the instrument specified in fits header)
+# 2- Allows the user to click on the image to select points
+# 3- Converts the selected pixel values to Carrington coordinates using the WCS information in the .fits file and Sunpy built in functions
+# 4- Saves the selected points to a .csv file
+
+import sunpy.map
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+import matplotlib.colors as colors
+import os
+
+class SelectImgPoints:
+    def __init__(self, fits_files, output_file, diff=False, coord_type='heliographic_stonyhurst', roi=None, overwrite=False, color_scl=None): # heliographic_carrington or heliographic_stonyhurst
+        self.fits_files = fits_files
+        self.output_file = output_file
+        self.points = []
+        self.diff = diff
+        self.coord_type = coord_type
+        self.roi = roi
+        self.overwrite = overwrite
+        self.color_scl = color_scl
+        # create odirectory if it does not exist
+        os.makedirs(os.path.dirname(output_file), exist_ok=True)
+        if os.path.isfile(self.output_file) and not self.overwrite:
+            print(f'Error: File {self.output_file} already exists. Use overwrite=True to overwrite it')
+            os._exit(0)        
+
+    def select_points(self):
+        if self.diff:
+        # if diff, then substract two consecutive files and computes the difference before plotting and selecting points
+            for i in range(len(self.fits_files)//2-1):
+                print('Reading image '+ str(i+1) + ' of ' + str(len(self.fits_files)//2-1))
+                # Read the .fits file as a Sunpy MAPS object
+                map_seq = sunpy.map.Map([self.fits_files[2*i], self.fits_files[2*i+1]], sequence=True)
+                #checks compatible image sizes
+                if map_seq[0].data.shape != map_seq[1].data.shape:
+                    print(f'Warning: {self.fits_files[2*i]} and {self.fits_files[2*i+1]} have different image sizes')
+                    # resamples to match the smallest image
+                    if map_seq[0].data.shape[0] < map_seq[1].data.shape[0]:
+                        res_map1 = map_seq[1].resample(map_seq[0].data.shape * u.pixel)
+                        map_seq = sunpy.map.Map([map_seq[0], res_map1], sequence=True)
+                    else:
+                        res_map0 = map_seq[0].resample(map_seq[1].data.shape * u.pixel)
+                        map_seq = sunpy.map.Map([res_map0,map_seq[1]], sequence=True)                                              
+                # Plot the image
+                # uses the name of the two files to name the plot
+                title = self.fits_files[2*i+1].split('/')[-1]  + ' - ' +  self.fits_files[2*i].split('/')[-1] 
+                map = sunpy.map.Map(map_seq[1].quantity - map_seq[0].quantity, map_seq[0].meta)
+                # if roi is specified, plot only that portion of the map
+                if self.roi is not None:
+                    top_right = SkyCoord(self.roi[0]*u.arcsec, self.roi[1]*u.arcsec, frame=map.coordinate_frame)
+                    bottom_left = SkyCoord(self.roi[2]*u.arcsec, self.roi[3]*u.arcsec, frame=map.coordinate_frame)
+                    map = map.submap(bottom_left, top_right=top_right)
+                    print(f'Warning: ROI box [arcsec] specified. Plotting only the portion of the image within the box {self.roi}')
+                m = np.mean(map.data)
+                sd = np.std(map.data)
+                if self.color_scl is None: 
+                    scl = 3
+                else:
+                    scl = self.color_scl 
+                # plots with white as max and black as min
+                map.plot(norm=colors.Normalize(vmin=m-scl*sd, vmax=m+scl*sd), cmap='gray', title=title)
+                # Allow the user to click on the image to select points
+                points = plt.ginput(n=-1, timeout=0)
+                # Convert the selected pixel values to Carrington coordinates using the WCS information in the .fits file and Sunpy built in functions
+                wcs = map.wcs
+                wcs_points= [wcs.pixel_to_world(j[0], j[1]) for j in points]
+                carrington_points = [SkyCoord(j.Tx, j.Ty, frame=self.coord_type, obstime=map.date) for j in wcs_points]
+                carrington_lon = [j.lon.arcsec for j in carrington_points]
+                carrington_lat = [j.lat.arcsec for j in carrington_points]
+                # Save the selected points to a common list including the file basename
+                self.points.append([self.fits_files[2*i].split('/')[-1], carrington_lon, carrington_lat])
+                plt.close()
+                # # prompts the user if they want to continue selecting points or stop
+                # if i < len(self.fits_files)-2:
+                #     cont = input('Do you want to continue selecting points? (y/n): ')
+                #     if cont.lower() == 'n':
+                #         break
+            # Save the selected points to a .csv file   
+            df = pd.DataFrame(self.points, columns=['file', 'lon [arcsec]', 'lat [arcsec]'])
+            df.to_csv(self.output_file, index=False)
+        else:
+            for fits_file in self.fits_files:
+                # Read the .fits file as a Sunpy MAPS object
+                map = sunpy.map.Map(fits_file)
+                # Plot the image
+                map.plot()
+                # Allow the user to click on the image to select points
+                points = plt.ginput(n=-1, timeout=0)
+                # Convert the selected pixel values to Carrington coordinates using the WCS information in the .fits file and Sunpy built in functions
+                wcs = map.wcs
+                wcs_points= [wcs.pixel_to_world(i[0], i[1]) for i in points]
+                carrington_points = [SkyCoord(i.Tx, i.Ty, frame=self.coord_type, obstime=map.date) for i in wcs_points]
+                carrington_lon = [i.lon.arcsec for i in carrington_points]
+                carrington_lat = [i.lat.arcsec for i in carrington_points]
+                # Save the selected points to a common list including the file basename
+                self.points.append([fits_file.split('/')[-1], carrington_lon, carrington_lat])
+                plt.close()
+                # # prompts the user if they want to continue selecting points or stop
+                # if i < len(self.fits_files)-2:
+                #     cont = input('Do you want to continue selecting points? (y/n): ')
+                #     if cont.lower() == 'n':
+                #         break                
+            # Save the selected points to a .csv file   
+            df = pd.DataFrame(self.points, columns=['file', 'lon [arcsec]', 'lat [arcsec]'])
+            df.to_csv(self.output_file, index=False)
---
+++ select_points_img_points_run.py
@@ -0,0 +1,49 @@
+
+from select_img_points import SelectImgPoints
+import pandas as pd
+import os
+import numpy as np
+
+#CONSTANTS
+repo_path = os.getcwd() 
+data_path = '/gehme/data/solo/fsi'
+master_list = repo_path + '/FSI_CMEbubbles_Onlytraceable.csv'
+id_of_events_to_process = [1]
+odir = repo_path + '/output'
+overwrite = False # if True, the output file will be overwritten if it already exists
+color_scl = 3 # number of sigmas to control image color scale range
+roi = None # Region of interest in arcsec [top_right_x, top_right_y, bottom_left_x, bottom_left_y]
+#######
+# reads master list
+output_file = odir + '/selected_points.csv'
+os.makedirs(os.path.dirname(output_file), exist_ok=True)
+
+master = pd.read_csv(master_list)
+for id in id_of_events_to_process:
+    # get start and end dates for the event id
+    start_date = master[master['id'] == id]['start_date'].values[0]
+    end_date = master[master['id'] == id]['end_date'].values[0]
+    #convert dates that are in format YYYY-MM-DD HH:mm to timestamp
+    start_date = pd.to_datetime(start_date)
+    end_date = pd.to_datetime(end_date)
+    # search in data_path for files that are within the start and end dates
+    # file structure in data_path is data_path/YYYY/MM/DD/solo_L2_eui-fsi304-image_YYYYMMDDTHHmmSSSSS_V01.fits*
+    fits_files = []
+    fits_files_dates = []
+    for root, dirs, files in os.walk(data_path):
+        for file in files:
+            if file.endswith('.fits'):
+                file_date = pd.to_datetime(file.split('_')[-2][0:15])
+                fits_files_dates.append(file_date)
+                if file_date >= start_date and file_date <= end_date:
+                    fits_files.append(os.path.join(root, file))
+    
+    #sort files by date
+    fits_files = [x for _,x in sorted(zip(fits_files_dates,fits_files))]
+
+# Create an instance of the SelectImgPoints that selects points on the images and saves them to a .csv file
+select_img_points = SelectImgPoints(fits_files, output_file, diff=True, roi=roi, overwrite=overwrite, color_scl=color_scl)
+# Select points
+select_img_points.select_points()
+# Print the path of the output file
+print(f'Points saved to {output_file}')
