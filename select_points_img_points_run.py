1e05e6ef84c677bfbaf2fb80a9c4fca3b97e9493
Francisco A. Iglesias <franciscoaiglesias@gmail.com>
2024-08-28 09:21:10 -0300

    added min timedelta for running dif

--- select_points_img_points_run.py
+++ select_points_img_points_run.py
@@ -8,13 +8,14 @@ import numpy as np
 repo_path = os.getcwd() 
 data_path = '/gehme/data/solo/fsi'
 master_list = repo_path + '/FSI_CMEbubbles_Onlytraceable.csv'
-id_of_events_to_process = [22]
+id_of_events_to_process = [1]
 odir = repo_path + '/output'
 overwrite = False # if True, the output file will be overwritten if it already exists
 color_scl = 3 # number of sigmas to control image color scale range
 roi = None # Region of interest in arcsec [top_right_x, top_right_y, bottom_left_x, bottom_left_y]
 exp_scl = 0.2 # Exponential scaling factor for the image
 files_names_to_avoid = ['fsi304'] # any filename containing any of these strings will be avoided
+min_time_delta = 60.*9. # minimum time delta between consecutive images in seconds.
 #######
 
 # reads master list
@@ -41,22 +42,32 @@ for id in id_of_events_to_process:
                 file_date = pd.to_datetime(file.split('_')[-2][0:15])
                 if file_date >= start_date and file_date <= end_date:
                     fits_files.append(os.path.join(root, file))
-                    fits_files_dates.append(file_date)               
+                    fits_files_dates.append(file_date)  
+    #sort files by timestamp
+    ind = np.argsort(fits_files_dates)
+    fits_files = [fits_files[i] for i in ind]
+    fits_files_dates = [fits_files_dates[i] for i in ind]                    
+    print('All files found... %s' % len(fits_files))
+    [print(f) for f in fits_files]                                 
     # remove files that contain any of the strings in files_names_to_avoid
     ind_to_keep = np.ones(len(fits_files), dtype=bool)
     for f in files_names_to_avoid:
         ind_to_keep = ind_to_keep & ~np.array([f in file for file in fits_files])
     fits_files = [fits_files[i] for i in np.where(ind_to_keep)[0]]
     fits_files_dates = [fits_files_dates[i] for i in np.where(ind_to_keep)[0]]
-    #sort files by timestamp
-    ind = np.argsort(fits_files_dates)
-    fits_files = [fits_files[i] for i in ind]
-print('Files to be processed...')
-[print(f) for f in fits_files]
-# Create an instance of the SelectImgPoints that selects points on the images and saves them to a .csv file
-select_img_points = SelectImgPoints(fits_files, output_file, diff='consecutive_diff',norm_exp=True,
+    # drop one of the files if the time delta between consecutive files is less than min_time_delta
+    time_deltas = np.diff(fits_files_dates)
+    ind_to_keep = np.ones(len(fits_files), dtype=bool)
+    for i, td in enumerate(time_deltas):
+        if td < pd.Timedelta(seconds=min_time_delta):
+            ind_to_keep[i+1] = False
+    fits_files = [fits_files[i] for i in np.where(ind_to_keep)[0]]
+    print('Files to be processed... %s' % len(fits_files))
+    [print(f) for f in fits_files]
+    # Create an instance of the SelectImgPoints that selects points on the images and saves them to a .csv file
+    select_img_points = SelectImgPoints(fits_files, output_file, diff='consecutive_diff',norm_exp=True,
                                     exp_scl=exp_scl, roi=roi, overwrite=overwrite, color_scl=color_scl)
-# Select points
-select_img_points.select_points()
-# Print the path of the output file
-print(f'Points saved to {output_file}')
+    # Select points
+    select_img_points.select_points()
+    # Print the path of the output file
+    print(f'Points saved to {output_file}')
