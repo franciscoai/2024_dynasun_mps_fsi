1ae44bb06f07b93c5e258a3d0cf3d148ce0e5fc3
Francisco A. Iglesias <franciscoaiglesias@gmail.com>
2024-08-30 08:48:30 -0300

    added few nice plots

--- analyze_points.py
+++ analyze_points.py
@@ -20,6 +20,8 @@ points_lists = sorted([os.path.join(points_list_path, f) for f in points_lists])
 # variables to plot together for all csv files
 all_mean_dawdh = []  
 all_mean_spped = []
+all_mean_aw = []
+all_aw_fit_par = []
 # process each csv file
 for points_list in points_lists:
     print('Computing derived quantities for file: ' + os.path.basename(points_list))
@@ -55,15 +57,25 @@ for points_list in points_lists:
         all_phi = np.arctan2(all_lat, all_lon)
         all_r = np.sqrt(all_lat**2 + all_lon**2)
         # computes the angular width between the first and third point
-        # checks if the points are in the same quadrant
         if all_phi[0]*all_phi[2] > 0:
             aw.append(np.abs(all_phi[0]-all_phi[2])*180/np.pi)
         else:
-            aw.append(np.abs(all_phi[0]+all_phi[2])*180/np.pi)
+            abs_sum = np.abs(np.abs(all_phi[2]) + np.abs(all_phi[0])) 
+            if abs_sum > np.pi:
+                aw.append(np.abs(abs_sum - 2*np.pi)*180/np.pi)
+            else:
+                aw.append(abs_sum*180/np.pi)
+            
         # converts all_r[1] from arcsec to solar radii
         h_rsun = np.deg2rad(all_r[1]/3600)*dsun[i]/solar_rad_in_m
         h.append(h_rsun)
 
+    event_id = str.split(os.path.basename(curr_output_dir),'_')[-1]
+    all_mean_aw.append([event_id, np.mean(aw)])
+
+    # if event_id == 'id01':
+    #     breakpoint()
+    
     print('Plotting...')
     # plot aw vs date to png file
     fig = plt.figure()
@@ -94,7 +106,6 @@ for points_list in points_lists:
     h_km = np.array(h)*solar_rad_in_m/1e3
     date_diff_sec = np.diff(points['date']).astype('timedelta64[s]').astype(int)
     speed = np.diff(h_km)/date_diff_sec
-    event_id = str.split(os.path.basename(curr_output_dir),'_')[-1]
     all_mean_spped.append([event_id, np.mean(speed)])
     plt.plot(points['date'][1:], speed,'o--k')
     plt.xticks(rotation=45)
@@ -108,18 +119,22 @@ for points_list in points_lists:
     # plot aw vs h to png file
     fig = plt.figure()
     plt.plot(h, aw,'o--k')
+    # fit and plot a line
+    m, b = np.polyfit(h, aw, 1)
+    all_aw_fit_par.append([event_id, m, b])
+    plt.plot(h, m*np.array(h) + b, 'r',label='y = ' + str(round(m,2)) + 'x + ' + str(round(b,2)))
     plt.ylabel('Angular width [deg]')
     plt.xlabel('Height [Rs]')
     plt.title('Angular width vs Height')
     plt.tight_layout()
     plt.grid()
+    plt.legend()
     plt.savefig(curr_output_dir + '/aw_vs_h.png')
     plt.close()
 
     # plot derivative of aw wrt h vs h
     fig = plt.figure()
     dawdh = np.diff(aw)/np.diff(h)
-    event_id = str.split(os.path.basename(curr_output_dir),'_')[-1]
     all_mean_dawdh.append([event_id, np.mean(dawdh)])
     plt.plot(h[1:], dawdh,'o--k')
     plt.ylabel('d(aw)/dh [deg/Rs]')
@@ -131,18 +146,18 @@ for points_list in points_lists:
     plt.close()
 
 # common plots
-# plot all mean dawdh vs event_id
-fig = plt.figure()
-all_mean_dawdh = np.array(all_mean_dawdh)
-plt.plot(all_mean_dawdh[:,0], all_mean_dawdh[:,1].astype(float),'o--k')
-plt.xticks(rotation=45)
-plt.ylabel('Mean d(aw)/dh [deg/Rs]')
-plt.xlabel('Event ID')
-plt.title('Mean d(aw)/dh vs Event ID')
-plt.tight_layout()
-plt.grid()
-plt.savefig(output_dir + '/mean_dawdh_vs_event_id.png')
-plt.close()
+# # plot all mean dawdh vs event_id
+# fig = plt.figure()
+# all_mean_dawdh = np.array(all_mean_dawdh)
+# plt.plot(all_mean_dawdh[:,0], all_mean_dawdh[:,1].astype(float),'o--k')
+# plt.xticks(rotation=45)
+# plt.ylabel('Mean d(aw)/dh [deg/Rs]')
+# plt.xlabel('Event ID')
+# plt.title('Mean d(aw)/dh vs Event ID')
+# plt.tight_layout()
+# plt.grid()
+# plt.savefig(output_dir + '/mean_dawdh_vs_event_id.png')
+# plt.close()
 
 # plot all mean speed vs event_id
 fig = plt.figure()
@@ -157,7 +172,71 @@ plt.grid()
 plt.savefig(output_dir + '/mean_speed_vs_event_id.png')
 plt.close()
 
+# plot all mean aw vs event_id
+fig = plt.figure()
+all_mean_aw = np.array(all_mean_aw)
+plt.plot(all_mean_aw[:,0], all_mean_aw[:,1].astype(float),'o--k')
+plt.xticks(rotation=45)
+plt.ylabel('Mean Angular width [deg]')
+plt.xlabel('Event ID')
+plt.title('Mean Angular width vs Event ID')
+plt.tight_layout()
+plt.grid()
+plt.savefig(output_dir + '/mean_aw_vs_event_id.png')
+plt.close()
 
+#plot all all_aw_fit_par vs event_id
+fig = plt.figure()
+all_aw_fit_par = np.array(all_aw_fit_par)
+plt.plot(all_aw_fit_par[:,0], all_aw_fit_par[:,1].astype(float),'o--k',label='m')
+plt.plot(all_aw_fit_par[:,0], all_aw_fit_par[:,2].astype(float),'o--r',label='b')
+plt.xticks(rotation=45)
+plt.ylabel('Fit parameters')
+plt.xlabel('Event ID')
+plt.title('Fit parameters vs Event ID')
+plt.tight_layout()
+plt.grid()
+plt.legend()
+plt.savefig(output_dir + '/fit_par_vs_event_id.png')
+plt.close()
+
+# scatter plot of all_aw_fit_par[:,1] vs all_aw_fit_par[:,2]
+fig = plt.figure()
+plt.scatter(all_aw_fit_par[:,1].astype(float), all_aw_fit_par[:,2].astype(float))
+plt.ylabel('b')
+plt.xlabel('m')
+plt.title('b vs m')
+plt.tight_layout()
+plt.grid()
+plt.savefig(output_dir + '/b_vs_m.png')
+plt.close()
+
+# scatter plot of all_aw_fit_par[:,1] vs all_aw_fit_par[:,2]
+fig = plt.figure()
+# computes the fit value at h=1
+aw_at_1rs = all_aw_fit_par[:,1].astype(float) + all_aw_fit_par[:,2].astype(float)
+plt.scatter(all_aw_fit_par[:,1].astype(float), aw_at_1rs)
+plt.ylabel('aw at 1 Rs [deg]')
+plt.xlabel('mean daw/dh [deg/Rs]')
+plt.tight_layout()
+plt.grid()
+plt.savefig(output_dir + '/aw_at_1rs_vs_dawdh.png')
+plt.close()
+print('Done!')
+
+# scatter plot of all_aw_fit_par[:,1] vs all_aw_fit_par[:,2]
+fig = plt.figure()
+# computes the fit value at h=1
+aw_at_1rs = all_aw_fit_par[:,1].astype(float)*1.25 + all_aw_fit_par[:,2].astype(float)
+plt.scatter(all_aw_fit_par[:,1].astype(float), aw_at_1rs)
+# fit a 
+plt.ylabel('aw at 1.25 Rs [deg]')
+plt.xlabel('mean daw/dh [deg/Rs]')
+plt.tight_layout()
+plt.grid()
+plt.savefig(output_dir + '/aw_at_1.25rs_vs_dawdh.png')
+plt.close()
+print('Done!')
 
 
 
